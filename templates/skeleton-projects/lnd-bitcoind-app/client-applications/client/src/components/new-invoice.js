b1010e14c86923e5ff4df7a9623d17feb62cf6f2
pwkad <patrickwaltersc21@yahoo.com>
2018-10-16 19:21:50 -0500

    Fix bad reference

--- templates/skeleton-projects/lnd-bitcoind-app/client-applications/client/src/components/new-invoice.js
+++ templates/skeleton-projects/lnd-bitcoind-app/client-applications/client/src/components/new-invoice.js
@@ -1,6 +1,5 @@
 import {bindable} from 'aurelia-templating';
 import {InvoicesService} from 'services/invoices';
-import {Invoice} from 'models/invoices';
 
 export class NewInvoice {
   @bindable invoice;
