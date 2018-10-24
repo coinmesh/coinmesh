d9697d9ff9b091ecf91e9604fb3a05c90f2204e9
pwkad <patrickwaltersc21@yahoo.com>
2018-10-24 11:11:19 -0500

    Add temporary copy scripts for updating adapters

---
+++ copy-adapters-down.sh
@@ -0,0 +1,7 @@
+cp -R ./adapters/lnd-adapter/* ./templates/skeleton-projects/lnd-bitcoind-app/adapters/lnd-adapter/
+cp -R ./adapters/bitcoind-adapter/* ./templates/skeleton-projects/lnd-bitcoind-app/adapters/bitcoind-adapter/
+
+cp -R ./adapters/bitcoind-adapter/* ./templates/skeleton-projects/bitcoind-app/adapters/bitcoind-adapter/
+
+cp -R ./adapters/litecoind-adapter/* ./templates/skeleton-projects/litecoind-app/adapters/litecoind-adapter/
+
---
+++ copy-adapters-up.sh
@@ -0,0 +1,7 @@
+cp -R  ./templates/skeleton-projects/lnd-bitcoind-app/adapters/lnd-adapter/* ./adapters/lnd-adapter/
+cp -R  ./templates/skeleton-projects/lnd-bitcoind-app/adapters/bitcoind-adapter/* ./adapters/bitcoind-adapter/
+
+cp -R  ./templates/skeleton-projects/bitcoind-app/adapters/bitcoind-adapter/* ./adapters/bitcoind-adapter/
+
+cp -R  ./templates/skeleton-projects/litecoind-app/adapters/litecoind-adapter/* ./adapters/litecoind-adapter/
+
