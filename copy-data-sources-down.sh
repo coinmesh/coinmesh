d711fb36e51aa76535ca86a91945afa4046c7a6e
pwkad <patrickwaltersc21@yahoo.com>
2018-10-25 15:12:15 -0500

    Update all data sources

---
+++ copy-data-sources-down.sh
@@ -0,0 +1,7 @@
+cp -R ./data-sources/lnd/* ./templates/skeleton-projects/lnd-bitcoind-app/data-sources/lnd/
+cp -R ./data-sources/bitcoind/* ./templates/skeleton-projects/lnd-bitcoind-app/data-sources/bitcoind/
+
+cp -R ./data-sources/bitcoind/* ./templates/skeleton-projects/bitcoind-app/data-sources/bitcoind/
+
+cp -R ./data-sources/litecoind/* ./templates/skeleton-projects/litecoind-app/data-sources/litecoind/
+
---
+++ copy-data-sources-up.sh
@@ -0,0 +1,6 @@
+cp -R ./templates/skeleton-projects/lnd-bitcoind-app/data-sources/lnd/* ./data-sources/lnd/
+cp -R ./templates/skeleton-projects/lnd-bitcoind-app/data-sources/bitcoind/* ./data-sources/bitcoind/
+
+cp -R ./templates/skeleton-projects/bitcoind-app/data-sources/bitcoind/* ./data-sources/bitcoind/
+
+cp -R ./templates/skeleton-projects/litecoind-app/data-sources/litecoind/* ./data-sources/litecoind/
