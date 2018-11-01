995b240eed9af332f2a73aaf8c423efa27518356
pwkad <patrickwaltersc21@yahoo.com>
2018-11-01 14:35:22 -0500

    Few updates

--- adapters/gemini-adapter/index.js
+++ adapters/gemini-adapter/index.js
@@ -1,5 +1,13 @@
+const addressesService = new (require('./services/addresses'));
+const balancesService = new (require('./services/balances'));
 const ordersService = new (require('./services/orders'));
+const publicApisService = new (require('./services/public-apis'));
+const transfersService = new (require('./services/transfers'));
 
 module.exports = {
- ordersService
+  addressesService,
+  balancesService,
+  ordersService,
+  publicApisService,
+  transfersService
 };
