6898f5f689e78eafe30f3ce8fd1ddd45e9c50eb9
pwkad <patrickwaltersc21@yahoo.com>
2019-01-18 12:37:47 -0600

    Adding other RPCs

--- adapters/bitcoind-adapter/services/transactions.js
+++ adapters/bitcoind-adapter/services/transactions.js
@@ -1,11 +1,58 @@
 const { JsonRpcRequest, jsonRpcClient } = require('../services/json-rpc');
 
 class TransactionsService {
-  getUnspentByAddress(address, minConfs = 1) {
+  getUnspentByAddress(addresses) {
+    if (!addresses) {
+      throw new Error('addresses parameter required.');
+    }
+    if (!addresses &&!Array.isArray(addresses)) {
+      addresses = [addresses];
+    }
+
     let request = {
       method: 'listunspent',
-      params: [minConfs, null, address],
+      params: [1, null, addresses],
       id: 'listunspent'
+    };
+
+    return jsonRpcClient.post(request);
+  }
+
+  createRawTransaction(inputs, outputs, locktime = null) {
+    let request = {
+      method: 'createrawtransaction',
+      params: [inputs, outputs, locktime],
+      id: 'createrawtransaction'
+    };
+
+    return jsonRpcClient.post(request);
+  }
+
+  decodeRawTransaction(transactionHexString) {
+    let request = {
+      method: 'decoderawtransaction',
+      params: [transactionHexString],
+      id: 'decoderawtransaction'
+    };
+
+    return jsonRpcClient.post(request);
+  }
+
+  signRawTransaction(transactionHexString) {
+    let request = {
+      method: 'signrawtransaction',
+      params: [transactionHexString],
+      id: 'signrawtransaction'
+    };
+
+    return jsonRpcClient.post(request);
+  }
+
+  sendRawTransaction(transactionHexString) {
+    let request = {
+      method: 'sendrawtransaction',
+      params: [transactionHexString, false],
+      id: 'sendrawtransaction'
     };
 
     return jsonRpcClient.post(request);
@@ -33,3 +80,4 @@ class TransactionsService {
 }
 
 module.exports = TransactionsService;
+
