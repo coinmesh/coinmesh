e50c3ad787761461a749598c373319d30fcfcea0
pwkad <patrickwaltersc21@yahoo.com>
2019-01-19 18:10:20 -0600

    Add getNode to lnd-adapter

--- adapters/lnd-adapter/services/network-info.js
+++ adapters/lnd-adapter/services/network-info.js
@@ -1,6 +1,7 @@
 const lnd = require('./lnd');
 const getNetworkGraph = require('ln-service/getNetworkGraph');
 const getNetworkInfo = require('ln-service/getNetworkInfo');
+const getNode = require('ln-service/getNode');
 
 class NetworkInfoService {
   getNetworkGraph() {
@@ -9,6 +10,10 @@ class NetworkInfoService {
   getNetworkInfo() {
     return getNetworkInfo({lnd});
   }
+  getNodeInfo(pubKey) {
+    return getNode({lnd, public_key: pubKey});
+  }
 }
 
 module.exports = NetworkInfoService;
+
